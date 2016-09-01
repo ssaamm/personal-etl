f97ad689065eb53aaffac3d1c3f6d7f79a0d598b
Samuel Taylor <github@samueltaylor.org>
2016-09-01 18:33:38 -0500

    Add workout data

---
+++ duration.py
@@ -0,0 +1,38 @@
+class Duration(object):
+    MINUTES_PER_HOUR = 60
+    SECONDS_PER_MINUTE = 60
+
+    def __init__(self, hours, minutes, seconds):
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds
+
+        if self.seconds >= Duration.SECONDS_PER_MINUTE:
+            num_minutes = self.seconds / Duration.SECONDS_PER_MINUTE
+            self.minutes += num_minutes
+            self.seconds -= num_minutes * Duration.SECONDS_PER_MINUTE
+
+        if self.minutes >= Duration.MINUTES_PER_HOUR:
+            num_hours = self.minutes / Duration.MINUTES_PER_HOUR
+            self.hours += num_hours
+            self.minutes -= num_hours * Duration.MINUTES_PER_HOUR
+
+    def __add__(self, other):
+        return Duration(self.hours + other.hours, self.minutes + other.minutes, self.seconds + other.seconds)
+
+    def __radd__(self, other):
+        if other == 0:
+            return Duration(self.hours, self.minutes, self.seconds)
+        else:
+            return self + other
+
+    def __eq__(self, other):
+        return self.hours == other.hours and self.minutes == other.minutes and self.seconds == other.seconds
+
+    def __repr__(self):
+        return '{h}:{m}:{s}'.format(h=self.hours, m=self.minutes, s=self.seconds)
+
+    @staticmethod
+    def from_string(duration_str):
+        hours, minutes, seconds = [int(s) for s in duration_str.split(':')]
+        return Duration(hours, minutes, seconds)
---
+++ workouts.py
@@ -0,0 +1,81 @@
+from common import gc
+from duration import Duration
+import datetime
+from itertools import islice, count, groupby
+import collections
+import sys
+
+from math_utils import rolling_average
+
+key = '1RzVbHFsIw6K9koiI9oGdWkhQmZrwUikfLIoNhvdlEs8'
+sheet = gc.open_by_key(key).get_worksheet(0)
+
+SEP = '\t'
+
+Exercise = collections.namedtuple('Exercise', ['timestamp', 'exercise', 'duration', 'distance', 'sets', 'reps', 'weight'])
+
+def get_raw_rows():
+    all_cols = [sheet.col_values(ndx) for ndx in range(1, 9)]
+
+    for row in islice(zip(*all_cols), 1, None):
+        if all(not v for v in row):
+            raise StopIteration
+
+        try:
+            latest_timestamp = parse_timestamp(row[0], row[1])
+        except ValueError:
+            pass
+
+        float_values = [float(row[i]) if row[i] else 0.0 for i in range(4, 8)]
+        try:
+            duration = Duration.from_string(row[3])
+        except ValueError:
+            duration = Duration(0, 0, 0)
+        yield Exercise(latest_timestamp, row[2], duration, *float_values)
+
+def get_daily_summary():
+    for date, exercises_grouped in groupby(get_raw_rows(), key=lambda t: t[0].date()):
+        exercises = list(exercises_grouped)
+        duration = sum(e.duration for e in exercises)
+        distance = sum(e.distance for e in exercises)
+        reps = sum(e.sets * e.reps for e in exercises)
+        yield date, len(exercises), duration, distance, reps
+
+def get_daily_did_i_workout():
+    workouts = {date: (num_exercises, duration, distance, reps)
+                for date, num_exercises, duration, distance, reps in get_daily_summary()}
+
+    chrono_dates = list(sorted(workouts.keys()))
+    first_day, last_day = min(chrono_dates), max(chrono_dates)
+
+    days = (first_day + datetime.timedelta(days=i) for i in range(1, (last_day - first_day).days + 1))
+    return [(d, d in workouts and any(workouts[d])) for d in days]
+
+def get_workout_rolling_avg(days=7):
+    did_workouts = get_daily_did_i_workout()
+    numbers = [int(b) for d, b in did_workouts]
+    for date_did_workout, avg in zip(did_workouts, rolling_average(numbers, window=days)):
+        yield date_did_workout[0], avg
+
+
+def get_headers():
+    return 'date', 'pct_last_wk_worked_out'
+
+def parse_timestamp(date, time):
+    return datetime.datetime.strptime('{0} {1}'.format(date, time), '%Y-%m-%d %H:%M')
+
+def get_data():
+    for i in get_workout_rolling_avg():
+        yield i
+
+if __name__ == '__main__':
+    sep = SEP
+    try:
+        if sys.argv[1] == '-s':
+            sep = sys.argv[2]
+    except IndexError:
+        pass
+
+    print(sep.join(get_headers()))
+    for data in get_data():
+        print(sep.join(str(i) for i in data))
