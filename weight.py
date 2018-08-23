933137e62a8d5bcf2097183629f2560b24f0c3fc
Samuel Taylor <git@samueltaylor.org>
2018-08-23 16:18:50 -0500

    Use a t distribution for weight CI

--- weight.py
+++ weight.py
@@ -10,7 +10,7 @@ key = '1RzVbHFsIw6K9koiI9oGdWkhQmZrwUikfLIoNhvdlEs8'
 sheet = gc.open_by_key(key).get_worksheet(1)
 
 
-CRIT_VAL_90 = 1.645
+CRIT_VAL_90 = [None, 6.314, 2.920, 2.353, 2.132, 2.015, 1.943, 1.895, 1.860, 1.833, 1.812, 1.796, 1.782, 1.771, 1.761]
 SEP = '\t'
 
 def get_raw_rows():
@@ -26,13 +26,13 @@ def get_data():
     df = pd.DataFrame(data=[t[1] for t in rows], index=[t[0] for t in rows], columns=['weight'])
     df = df.groupby(pd.TimeGrouper('1w')).weight.agg([np.mean, np.std, len])
 
-    ci = CRIT_VAL_90 * (df['std'] / np.sqrt(df['len']))
-    df['low'] = df['mean'] + ci
-    df['high'] = df['mean'] - ci
+    ci = df['len'].map(lambda n: CRIT_VAL_90[int(n - 1)]) * (df['std'] / np.sqrt(df['len']))
+    df['low'] = df['mean'] - ci
+    df['high'] = df['mean'] + ci
 
     df.dropna(inplace=True)
     df.index.name = 'date'
-    return df[['mean', 'low', 'high']]
+    return df[['mean', 'low', 'high']].iloc[-26:]
 
 def parse_timestamp(date, time):
     return datetime.strptime('{0} {1}'.format(date, time), '%m/%d/%Y %H:%M')
