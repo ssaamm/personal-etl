bbccf5a1124d7820fc1e0a66c7e042936de0198d
Samuel Taylor <git@samueltaylor.org>
2020-02-15 05:47:38 -0600

    Update Tweet From Trello to Python 3

--- tweet_from_trello.py
+++ tweet_from_trello.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     to_tweet = get_cards()
     card = next((c for c in to_tweet if len(c['name']) < 280), None)
     if card is None:
-        print 'no tweets'
+        print('no tweets')
         exit(1)
     status = card['name']
 
